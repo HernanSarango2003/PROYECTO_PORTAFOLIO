edebc90dc30d2cb6c954c5b4873e4a0ba8942820
Hernán <hsarangon@unemi.edu.ec>
2024-10-06 22:31:10 -0500

    Zorro el que lea esto

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -24,6 +24,9 @@ from portfolio import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
     path('blog/', include('blog.urls'))
 ]
 urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
